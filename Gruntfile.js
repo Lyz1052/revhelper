cd849e4de89e2cc64b1f97235f558ab84551cbf7
zhangliyi <258874554@qq.com>
2017-08-22 18:38:31 +0800

    1.0

---
+++ Gruntfile.js
@@ -0,0 +1,330 @@
+//revhelper
+
+//功能：为jsp中的js和css引用自动添加版本号，并发布到SVN
+//提示：
+//1.原始引用中的模板变量必须放在开头部位
+//2.HASH长度默认为8，如需修改，要更改gruntfile.js和filerev.js两个地方
+//3.filterIn方法必须返回不带模板或者参数的直接引用，例如${basePath}/folder/a.css，somewhere/b.js?v=1这类的引用都是不允许的，因为这样无法使用locater在filerev.summary中定位哈希后的文件
+
+
+//API：
+//init
+//初始化库，将当前版本作为上一次发布的版本
+
+//refresh （编译，更新资源引用）
+//将当前版本（src）复制到预发布目录（pub）下
+//比较上一个版本（latest）和预发布版本（pub）
+//删除pub中未发生变动的文件
+//在pub中，根据这些发生变动的文件，使用usemin更新jsp中的引用，列出所有改动过的引用和文件
+//将这些将修改过引用的文件更新回src中
+
+//postcommit（发布后回调）
+//将当前库中的资源文件作为最新版本的资源文件，复制到latest中
+//给latest中的资源文件添加版本
+
+//TODO
+//commit（自动提交SVN）
+//序列化  grunt.filerev.summary F:/usemintest/node_modules/grunt-usemin/tasks/usemin.js:40
+//序列化版本号，记录提交版本
+module.exports = function (grunt) {
+
+    var cfg = {
+        ext:{
+            res:'js,css',
+            page:'jsp,html'
+        },
+        //css和js引用中包含这些时，不替换引用
+        ignorePrefix:['http:','https:','${im_url}'],
+        //css和js引用的前缀
+        prefix:'${basePath}',
+        //
+        src:'WebRoot',
+        pub:'publishing',
+        latest:'latest',
+        tmp:'tmp',
+        hashlen:8,
+        filterIn:function(file){
+            //忽略不在项目中的资源
+            if(cfg.ignorePrefix.some(function(prefix){
+                    return file.indexOf(prefix)!=-1;
+                }))
+                return file;
+
+            file = file.replace(cfg.prefix,"");
+            if(file.indexOf('?')>-1){
+                file = file.substr(0,file.indexOf('?'));
+            }
+            return file.replace(cfg.prefix,"");
+        },
+        filterOut:function(file,fileObj,match,src){
+            //忽略不在项目中的资源
+            if(cfg.ignorePrefix.some(function(prefix){
+                    return file.indexOf(prefix)!=-1;
+                }))
+                return file;
+
+            var originSrc = file;
+            if(file.indexOf('?')>-1)
+                file = file.substring(0,file.indexOf('?'));
+
+            var parts = file.split(".");
+            var newname = file;
+            var hasNewVersion = false;
+            if(parts.length>2){
+                var version = parts[parts.length-2];
+                var regExp = new RegExp("^[a-zA-Z0-9]{"+cfg.hashlen+"}$","gm");
+                if(regExp.test(version)) {//xxx.hashcode.js ==> xxx.js?v=hashcode
+                    newname = parts.slice(0, parts.length - 2).join(".") + "." + parts[parts.length - 1] + "?v=" + version;
+                    hasNewVersion = true;
+                }
+            }
+
+            //locater没有定位到更新过版本的资源文件
+            if(!hasNewVersion){
+                return src;
+            }
+
+            newname = cfg.prefix+newname;
+            var replaced = match.replace(src, newname);
+            if(newname!=src){
+                grunt.revhelper = grunt.revhelper || {};
+                grunt.revhelper.log = grunt.revhelper.log || {};
+                var filePath = fileObj.dir+"/"+fileObj.name;
+                grunt.revhelper.log[filePath] = grunt.revhelper.log[filePath] || [];
+                grunt.revhelper.log[filePath].push({
+                    src:src,
+                    replaced:newname
+                })
+            }
+            return newname;
+        }
+    }
+
+
+    grunt.initConfig({
+        yeoman:cfg,
+        copy: {
+            publishing: {
+                files: [{
+                    expand: true,
+                    // dot: true,
+                    cwd: '<%= yeoman.src%>/',
+                    dest: '<%= yeoman.pub %>/',
+                    src: [
+                        '**/*.{<%= yeoman.ext.res %>}',
+                        '**/*.{<%= yeoman.ext.page %>}'
+                    ]
+                }]
+            },
+            comparing: {
+                files: [{
+                    expand: true,
+                    // dot: true,
+                    cwd: '<%= yeoman.latest%>/',
+                    dest: '<%= yeoman.pub %>/',
+                    deletesame: true,
+                    deleteskip:'<%= yeoman.ext.page %>',
+                    src: [
+                        '**/*.{<%= yeoman.ext.res %>}'
+                    ]
+                }]
+            },
+            latest: {
+                files: [{
+                    expand: true,
+                    // dot: true,
+                    cwd: '<%= yeoman.src%>/',
+                    dest: '<%= yeoman.latest %>/',
+                    src: [
+                        '**/*.{<%= yeoman.ext.page %>}',
+                        // '**/*.css',
+                    ]
+                }]
+            },
+            src: {
+                files: [{
+                    expand: true,
+                    // dot: true,
+                    cwd: '<%= yeoman.pub %>/',
+                    dest: '<%= yeoman.src%>/',
+                    src: [
+                        '**/*.{<%= yeoman.ext.page %>}',
+                    ]
+                }]
+            }
+        },
+
+        useminPrepare: {
+            html: '<%= yeoman.pub %>/**/*.{<%= yeoman.ext.page %>}',
+            options: {
+                dest: '<%= yeoman.tmp %>'
+            }
+        },
+
+        usemin: {
+            html: '<%= yeoman.pub %>/**/*.{<%= yeoman.ext.page %>}',
+            options: {
+                assetsDirs: ['<%= yeoman.pub%>/'],
+                patterns: {
+                    html: [
+                        [ /<script.+src=['"]([^"']+)["']/gm, 'Replaceing script',cfg.filterIn,cfg.filterOut],
+                        [ /<link[^\>]+href=['"]([^"']+)["']/gm, 'Replaceing script',cfg.filterIn,cfg.filterOut],
+                    ]
+                },
+            },
+        },
+
+        filerev: {
+            latest: {
+                src: [
+                    '<%= yeoman.latest %>/**/*.{<%= yeoman.ext.res %>}'
+                ]
+            },
+            publishing: {
+                src: [
+                    '<%= yeoman.pub %>/**/*.{<%= yeoman.ext.res %>}'
+                ]
+            }
+        }
+    })
+
+
+    //grunt.loadNpmTasks('grunt-rev');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('deldirs', function () {
+        if(grunt.file.exists(cfg.pub))
+            grunt.file.delete(cfg.pub);
+        if(grunt.file.exists(cfg.tmp))
+            grunt.file.delete(cfg.tmp);
+    })
+
+    grunt.registerTask('logrev', function () {
+        console.log("\n=========== Summary ===========\n")
+
+        //资源文件版本变更统计
+        if(grunt.filerev.summary){
+            var cnt = 1;
+            console.log("资源文件版本变更统计：")
+            var diffcount = Object.getOwnPropertyNames(grunt.filerev.summary).length;
+            if(diffcount){
+                for(file in grunt.filerev.summary){
+                    console.log(""+cnt+++"."+file);
+                }
+            }else{
+                console.log("当前库中的资源文件与上一次发布版本时的资源文件相同.")
+            }
+
+            console.log();
+        }
+
+        //资源引用更新统计
+        console.log("资源引用更新统计：")
+        if(grunt.revhelper&&grunt.revhelper.log){
+            var cnt = 1;
+            console.log("共对 "+Object.getOwnPropertyNames(grunt.revhelper.log).length+" 个文件中的资源引用进行了更新：")
+            console.log();
+            var cnt = 1;
+            for(filePath in grunt.revhelper.log){
+                console.log(""+cnt+++"."+filePath+"：");
+                grunt.revhelper.log[filePath].forEach(function(e){
+                    console.log("  "+e.src+" ===>> "+e.replaced);
+                })
+                console.log();
+            }
+            console.log();
+        }else{
+            console.log("当前库中的资源引用没有需要更新的地方.\n")
+        }
+
+        console.log("=========== Summary ===========")
+    })
+
+    grunt.registerTask('init', function () {
+        grunt.loadNpmTasks('grunt-filerev');
+
+        if(grunt.file.exists(cfg.latest))
+            grunt.file.delete(cfg.latest);
+
+        grunt.task.run([
+            'deldirs',
+            'copy:latest',
+            'filerev:latest'
+        ]);
+
+    });
+
+    grunt.registerTask('refresh', function (arg) {
+        grunt.revhelper = {};
+        grunt.loadNpmTasks('grunt-usemin');
+        grunt.loadNpmTasks('grunt-filerev');
+
+        console.log(arg)
+
+        grunt.task.run([
+            'deldirs',
+            'copy:publishing',
+            'filerev:publishing',
+            'copy:comparing',
+            'useminPrepare',
+            'usemin',
+            'logrev',
+            'copy:src',
+        ]);
+    });
+
+    grunt.registerTask('postcommit', function (arg) {
+        grunt.loadNpmTasks('grunt-filerev');
+
+        if(grunt.file.exists(cfg.latest))
+            grunt.file.delete(cfg.latest);
+
+        grunt.task.run([
+            'copy:latest',
+            'filerev:latest',
+        ]);
+    });
+
+    grunt.registerTask('commit', function () {
+        var svnUltimate = require('node-svn-ultimate');
+        var readline = require('readline');
+        var fs = require('fs');
+
+        // var rl = readline.createInterface({
+        //     input: process.stdin,
+        //     output: process.stdout
+        // });
+        // var ask = function(){
+        //
+        // process.stdin.pause();
+        // var response = fs.readSync(process.stdin.fd, 1000, 0, "utf8");
+        // process.stdin.resume();
+        // remark = response[0].trim();
+        //
+        //
+        //     // rl.question('提交SVN备注: ',function (remark)  {
+        //         if(!remark) {
+        //             console.log('备注不能为空！')
+        //         } else {
+        //             // grunt.task.run(['copy:src']);
+        //             console.log(remark);
+        //             svnUltimate.commands.commit(cfg.src, {
+        //                     trustServerCert: true,	// same as --trust-server-cert
+        //                     username: "username",	// same as --username
+        //                     password: "password",	// same as --password
+        //                     // shell: "sh", 			// override shell used to execute command
+        //                     // cwd: process.cwd(),		// override working directory command is executed
+        //                     params: ['-m "' + remark + '"']
+        //                 },
+        //                 function () {
+        //                     console.log('提交成功');
+        //                     grunt.task.run(['copy:latest']);
+        //                 });
+        //         }
+        //         // rl.close();
+        //     // });
+        // }
+        //
+        // ask();
+    });
+};
---
+++ README.md
@@ -0,0 +1,139 @@
+# Revhelper
+ 用途：自动更新项目中资源文件的引用
+ 目前只支持替换项目中的绝对路径引用
+# 依赖
+ - [Nodejs](http://nodejs.cn/download/)
+ - [grunt-cli](https://gruntjs.com/getting-started)
+ - [grunt-contrib-copy](https://github.com/gruntjs/grunt-contrib-copy/)
+ - [grunt-usemin](https://github.com/yeoman/grunt-usemin)
+ - [grunt-filerev](https://github.com/yeoman/grunt-filerev)
+ 
+---
+ 
+# 初始化
+
+## 安装[nodejs](http://nodejs.cn/download/)
+安装完成后可以使用`npm config set registry http://registry.npm.taobao.org/`命令使用国内镜像下载npm包，加快速度
+
+## 安装[grunt-cli](https://gruntjs.com/getting-started)
+```
+npm install -g grunt-cli
+```
+
+## 在项目目录下，安装grunt插件
+```
+npm install grunt-filerev --save-dev
+npm install grunt-usemin --save-dev
+npm install grunt-contrib-copy --save-dev
+```
+## 使用node_modules_modified中的内容覆盖node_modules
+
+## 拉取资源库（SVN）中Web资源目录的代码
+
+## 配置Gruntfile.js
+
+
+
+---
+
+# 配置（以默认配置为例）
+
+## revhelper配置
+```
+ var cfg = {
+         ext:{
+			//资源文件后缀名，逗号分割
+            res:'js,css',
+			//引用资源文件的页面文件后缀名，逗号分割
+            page:'jsp,html'
+        },
+        //忽略的引用：css和js引用中包含这些时，不替换引用
+        ignorePrefix:['http:','https:','${im_url}'],
+        //引用中绝对路径的前缀
+        prefix:'${basePath}',
+        //当前版本的资源目录，包含所有资源文件和引用所在的页面文件，可用SVN进行版本控制
+        src:'WebRoot',
+		//预发布目录
+        pub:'publishing',
+		//最新一次发布版本目录
+        latest:'latest',
+		//临时文件夹目录
+        tmp:'tmp',
+		//检测的文件哈希长度
+        hashlen:8,
+		//usemin工具的引用替换前过滤器
+		//file: 根据指定正则（options.patterns）寻找到的引用
+		//当正则是 /<script.+src=['"]([^"']+)["']/gm 时，
+		//如果页面中找到了 <script src="${basePath}/js/index.js?v=1">，
+		//那么file就是 ${basePath}/js/index.js?v=1
+        filterIn:function(file){
+			//return: 可识别的相对路径，上述情况时，返回值应该是 /js/index.js
+			return file;
+        },
+		//usemin工具的引用替换后过滤器
+		//file: 当前资源文件的地址
+		//fileObj: file的相关对象
+		//match：正则的匹配（全部匹配）
+		//src: 原始匹配
+        filterOut:function(file,fileObj,match,src){
+			...
+			//return：替换后的匹配
+            return newname;
+        }
+    }
+```
+
+## grunt-usemin 配置
+```
+ grunt.initConfig({
+	 ...
+	 usemin: {
+		//需要替换引用的页面
+		html: '<%= yeoman.pub %>/**/*.{<%= yeoman.ext.page %>}',
+		options: {
+			//资源文件路径
+			assetsDirs: ['<%= yeoman.pub%>/'],
+			patterns: {
+				html: [
+				    //pattern，匹配页面内容的正则，Replaceing script 是替换之后的控制台输出log信息
+					[ /<script.+src=['"]([^"']+)["']/gm, 'Replaceing script',cfg.filterIn,cfg.filterOut],
+					[ /<link[^\>]+href=['"]([^"']+)["']/gm, 'Replaceing script',cfg.filterIn,cfg.filterOut],
+				]
+			}
+		},
+	},
+	...
+ });
+```
+
+ ---
+
+# 使用流程：
+
+- 等待版本更新的节点
+	更新资源库，将当前版本（1.0）提交
+	grunt init
+- 等待下一次版本更新（2.0）
+	更新资源库
+	grunt refresh
+	提交 2.0 版本
+	grunt postcommit
+- 等待下一次版本更新（3.0）
+	更新资源库
+	grunt refresh
+	提交 3.0 版本
+	...
+ ---
+
+# API
+
+## grunt init
+执行初始化命令，清空相关文件夹，并将当前目录视为最新一次提交的版本
+
+## grunt refresh
+执行更新命令，对比当前目录和最新提交目录，比对差异，替换当前目录中更新过的资源文件引用，并且输出统计
+
+## grunt postcommit
+执行提交操作后，必须执行该操作，目的是记录当前资源文件版本，以用作之后提交的比对
+
+
